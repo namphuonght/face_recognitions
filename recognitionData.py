5edcb30f37d21591c3c47bd8bb0cb73365e15c7a
namphuonght <trannamphuong2k@gmail.com>
2020-06-22 12:21:46 +0700

    firts commit

---
+++ recognitionData.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+import os
+import sqlite3
+from PIL import Image
+
+#trainning hinh anh nhan dien voi Thu vien nhan dien khuon mat
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+recofnizer = cv2.face.LBPHFaceRecognizer_create()
+
+recofnizer.read(r'C:\Users\LAptop\PycharmProjects\doan3\Recognizer\TrainningData.yml')
+
+#lay thong tin cua nguoi trong database
+
+
+def getProfile(id):
+    conn = sqlite3.connect('E:\data_face_recognition.db')
+    query = query = "SELECT * FROM people WHERE ID="+str(id)
+    cusror = conn.execute(query)
+
+    profile = None
+    for row in cusror:
+        profile = row
+    conn.close()
+    return profile
+
+
+cap = cv2.VideoCapture(0)
+fontface = cv2.FONT_HERSHEY_SIMPLEX
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 225, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        id, confidence = recofnizer.predict(roi_gray)
+        if confidence<40:
+            profile = getProfile(id)
+            if(profile != None):
+                cv2.putText(frame, ""+str(profile[1]), (x+10, y+h+30), fontface, 1, (0, 255, 0), 2)
+        else:
+            cv2.putText(frame, "Unknow", (x + 10, y + h + 30), fontface, 1, (0, 255, 0), 2)
+
+    cv2.imshow('image', frame)
+    if(cv2.waitKey(1) == ord('q')):
+        break
+
+cap.release()
+cv2.destroyWindow()
+
