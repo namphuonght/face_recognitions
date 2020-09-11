ec2faa234cb804b3a38603a732a8fc99bcc237b5
namphuonght <trannamphuong2k@gmail.com>
2020-09-11 15:20:36 +0700

    update

--- recognitionData.py
+++ recognitionData.py
@@ -1,30 +1,29 @@
 import cv2
 import numpy as np
 import os
+import datetime
 import sqlite3
 from PIL import Image
 
 #trainning hinh anh nhan dien voi Thu vien nhan dien khuon mat
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 recofnizer = cv2.face.LBPHFaceRecognizer_create()
-
+kiemtra = False
 recofnizer.read(r'C:\Users\LAptop\PycharmProjects\doan3\Recognizer\TrainningData.yml')
-
+conn = sqlite3.connect('E:\data_face_recognition.db')
 #lay thong tin cua nguoi trong database
 
 
 def getProfile(id):
-    conn = sqlite3.connect('E:\data_face_recognition.db')
-    query = query = "SELECT * FROM people WHERE ID="+str(id)
+    query = "SELECT * FROM people WHERE ID="+str(id)
     cusror = conn.execute(query)
 
     profile = None
     for row in cusror:
         profile = row
-    conn.close()
     return profile
 
-
+datetime_object = datetime.datetime.now()
 cap = cv2.VideoCapture(0)
 fontface = cv2.FONT_HERSHEY_SIMPLEX
 while True:
@@ -39,13 +38,19 @@ while True:
             profile = getProfile(id)
             if(profile != None):
                 cv2.putText(frame, ""+str(profile[1]), (x+10, y+h+30), fontface, 1, (0, 255, 0), 2)
+                kiemtra = True
         else:
-            cv2.putText(frame, "Unknow", (x + 10, y + h + 30), fontface, 1, (0, 255, 0), 2)
-
+            cv2.putText(frame, "Unknow", (x + 10, y + h + 30), fontface, 1, (0, 0, 255), 2)
     cv2.imshow('image', frame)
     if(cv2.waitKey(1) == ord('q')):
         break
 
+if kiemtra == True:
+   conn.execute("INSERT INTO diemdanh(id_user, NgayDiemDanh) VALUES(" + str(profile[0]) + ", '" + str(datetime_object) + "')")
+   conn.commit()
+
+
 cap.release()
 cv2.destroyWindow()
+conn.close()
 
